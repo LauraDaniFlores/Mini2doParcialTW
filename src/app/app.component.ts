cb189a3c012730d964e20cb51717419b0f042be0
AdrianBalandran <adrianbala76@gmail.com>
2024-04-30 08:44:17 -0600

    Cambios en el diseño
    Se actualizo algunos cambios del encabezado, se agrego el favicon y se cambio el favicon.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CalendarModule } from 'primeng/calendar';
 import { ColorPickerModule } from 'primeng/colorpicker';
+import { EncabezadoComponent } from "../encabezado/encabezado.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet, CalendarModule, ColorPickerModule ],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, CalendarModule, ColorPickerModule, EncabezadoComponent]
 })
 export class AppComponent {
   title = 'KingAGS';
