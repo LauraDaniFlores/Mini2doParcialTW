563c9a5f8a26e15b8a0512e1318c2e0f6a724ba1
Luismtz31 <al292931@edu.uaa.mx>
2024-05-01 21:36:34 -0600

    bugs arreglados y pagina AcercaNosotros

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,14 @@ import { RouterOutlet } from '@angular/router';
 import { CalendarModule } from 'primeng/calendar';
 import { ColorPickerModule } from 'primeng/colorpicker';
 import { DatosEquipoComponent } from './datos-equipo/datos-equipo.component';
+import { AcercaNosotrosComponent } from './acerca-nosotros/acerca-nosotros.component';
+import { DomseguroPipe } from './domseguro.pipe';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CalendarModule, ColorPickerModule, DatosEquipoComponent ],
+  imports: [RouterOutlet, CalendarModule, ColorPickerModule, DatosEquipoComponent, AcercaNosotrosComponent, DomseguroPipe ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -16,5 +18,6 @@ export class AppComponent {
   title = 'KingAGS';
 
   color:string = '#fff'; 
+  video:string = 'Gic_CtkJSjE';
 }
 
