75d08bcc7f5b451c1a2fe4e09b001da5ce17ab39
LauraDaniFlores <laura.dani.flores@gmail.com>
2024-05-03 08:33:50 -0600

    Implementación del diseño y arreglo de fechas
    Cambio en interface reservaciones.ts con algunos tipos de datos.

--- src/app/reservaciones/reservaciones.component.ts
+++ src/app/reservaciones/reservaciones.component.ts
@@ -3,6 +3,7 @@ import { ListadoLSService } from '../service/listado-ls.service';
 import { Reservacion } from '../interface/reservaciones';
 import { Casa } from '../interface/casa';
 import { ImageModule } from 'primeng/image';
+import { Lugares } from '../lugares';
 
 @Component({
   selector: 'app-reservaciones',
@@ -16,12 +17,17 @@ export class ReservacionesComponent {
 
   listadoCasa!: Casa[]; 
   listadoReser!: Reservacion[]; 
+  lista:Casa[] = Lugares; 
+  today = new Date();
 
   constructor(private listadoLSService: ListadoLSService){}
   
   ngOnInit(){
     this.listadoCasa = this.listadoLSService.getLista(); 
+    this.listadoReser = this.listadoLSService.getlistaResers(); 
+    console.log(this.listadoReser);
     this.listadoLSService.imprimir(); 
+    console.log(this.today);
   }
 
   randomNumber(){
@@ -32,5 +38,40 @@ export class ReservacionesComponent {
     this.randomNumber(); 
     // return casa.imagen[this.random]; 
     return casa.imagen[0];
+  }
+  fechaAnterior(fechaI: Date){
+    return this.comprobarFecha(fechaI);
+  }
+  comprobarFecha(fecha: Date){
+    if(fecha.getFullYear() < this.today.getFullYear()){
+      return true; 
+    }else if(fecha.getFullYear() > this.today.getFullYear()){
+      return false;
+    }else if(fecha.getFullYear() == this.today.getFullYear()){
+      if(fecha.getMonth() < this.today.getMonth()){
+        return true;
+      }else if(fecha.getMonth() > this.today.getMonth()){
+        return false; 
+      }else if(fecha.getMonth() == this.today.getMonth()){
+        if(fecha.getDay() < this.today.getDay()){
+          return true; 
+        }else if(fecha.getDay() > this.today.getDay()){
+          return false; 
+        }else if(fecha.getMonth() == this.today.getMonth()){
+          return true; 
+        }
+      }
+    }
+    return true; 
+  }
+
+  sumarMonth(num: number){
+    return num+1; 
+  }
+  ponerCeros(num: number){
+    if(num <10){
+      return "0"+num; 
+    }
+    return num; 
   }
 }
--- src/app/service/listado-ls.service.ts
+++ src/app/service/listado-ls.service.ts
@@ -8,22 +8,74 @@ import { Lugares } from '../lugares';
   providedIn: 'root'
 })
 export class ListadoLSService {
-  listadoCasa!: Casa[]; 
-  listadoReser!: Reservacion[]; 
+  today = new Date();
+  antes:boolean = false;
+  listadoResers!: Reservacion[]; 
   lista:Casa[]=Lugares; 
+  li: Reservacion[] = [{
+    fechaI:new Date(2022, 11, 22),
+    fechaF:new Date(2022, 11, 25),
+    hora: '07:00',
+    dias: 3,
+    nombre: 'Laura',
+    telefono: '3461150701',
+    correo: 'danila@gmail.com',
+    precio: 945,
+    idR: 0,
+    idCasa: 0
+  }, 
+  {
+    fechaI: new Date(2023, 11, 1),
+    fechaF:new Date(2023, 11, 3),
+    hora: '07:00',
+    dias: 3,
+    nombre: 'Laura',
+    telefono: '3461150701',
+    correo: 'danila@gmail.com',
+    precio: 945,
+    idR: 0,
+    idCasa: 1
+  }]
 
 
   constructor() { 
-    this.listadoCasa = JSON.parse(localStorage.getItem("casas") || '[]'); 
-    this.listadoReser = JSON.parse(localStorage.getItem("reservaciones") || '[]'); 
+    this.listadoResers = JSON.parse(localStorage.getItem("reservaciones") || '[]'); 
   }
 
   getLista(){
     return this.lista; 
   }
+  getlistaResers(){
+    return this.li; 
+  }
+
+
+  agregarReservacion(reserv: Reservacion){
+    this.listadoResers.push(reserv);
+    localStorage.setItem("reservaciones", JSON.stringify(this.listadoResers)); 
+  }
+
+  nuevaReserva(): Reservacion{
+    return {
+      fechaI: new Date("0000, 00, 00"),
+      fechaF:new Date("0000, 00, 00"),
+      hora: '',
+      dias: 0,
+      nombre: '',
+      telefono: '',
+      correo: '',
+      precio: 0,
+      idR: this.listadoResers.length,
+      idCasa: 0
+    }
+  }
+
   
   imprimir(){
+    localStorage.setItem('reservaciones', JSON.stringify(this.li));
     localStorage.setItem('casas', JSON.stringify(this.lista));
-    console.log(this.lista); 
   }
+
+
+
 }
