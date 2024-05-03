f666eaa5545dbdc7e5d7579121a153565661b8e9
LauraDaniFlores <laura.dani.flores@gmail.com>
2024-05-02 20:53:05 -0600

    Lugares id agregado

--- src/app/lugares.ts
+++ src/app/lugares.ts
@@ -10,7 +10,8 @@ export const Lugares:Casa[]=[
         lugar: ['Zapopan','Jalisco', 'México'],
         anfitrion: 'Oscar',
         precio: 315,
-        ofrece: ['Cocina', 'Tv', 'Wifi']
+        ofrece: ['Cocina', 'Tv', 'Wifi'],
+        id: 0
     },
     {
         imagen: ['assets/imagenes/Casas/Aguascalientes/Aguas1.webp', 'assets/imagenes/Casas/Aguascalientes/Aguas2.webp', 'assets/imagenes/Casas/Aguascalientes/Aguas3.webp', 'assets/imagenes/Casas/Aguascalientes/Aguas4.webp'],
@@ -21,7 +22,8 @@ export const Lugares:Casa[]=[
         lugar: ['Aguascalientes','México', 'México'],
         anfitrion: 'Luis Salvador',
         precio: 326,
-        ofrece: ['Tv', 'Wifi', 'Refrigerador', 'Plancha']
+        ofrece: ['Tv', 'Wifi', 'Refrigerador', 'Plancha'],
+        id: 1
     },
     {
         imagen: ['assets/imagenes/Casas/Guadalajara/GuadalajaraCopy.png','assets/imagenes/Casas/Guadalajara/Guadalajara1.webp','assets/imagenes/Casas/Guadalajara/Guadalajara2.webp','assets/imagenes/Casas/Guadalajara/Guadalajara3.webp' ],
@@ -32,7 +34,8 @@ export const Lugares:Casa[]=[
         lugar: ['Guadalajara', 'Jalisco', 'México'],
         anfitrion: 'Gysell Alejandra',
         precio: 489,
-        ofrece: ['Wifi', 'Área para trabajar', 'Tv', 'Aire acondicionadao', 'Ascensor']
+        ofrece: ['Wifi', 'Área para trabajar', 'Tv', 'Aire acondicionadao', 'Ascensor'],
+        id: 2
     },
     {
         imagen: ['assets/imagenes/Casas/Aguascalientes2/Aguas1.webp','assets/imagenes/Casas/Aguascalientes2/Aguas2.jpeg','assets/imagenes/Casas/Aguascalientes2/Aguas3.jpeg', 'assets/imagenes/Casas/Aguascalientes2/Aguas4.webp','assets/imagenes/Casas/Aguascalientes2/Aguas5.webp' ],
@@ -43,7 +46,8 @@ export const Lugares:Casa[]=[
         lugar: ['Aguascalientes', 'México', 'México'],
         anfitrion: 'Brenda',
         precio: 1496,
-        ofrece: ['Cocina', 'Wifi', 'Tv', 'Plancha', 'Horno', 'Licuadora']
+        ofrece: ['Cocina', 'Wifi', 'Tv', 'Plancha', 'Horno', 'Licuadora'],
+        id: 3
     },
     {
         imagen: ['assets/imagenes/Casas/Mazatlan/Mazatlan1.webp','assets/imagenes/Casas/Mazatlan/Mazatlan2.webp', 'assets/imagenes/Casas/Mazatlan/Mazatlan3.jpeg', 'assets/imagenes/Casas/Mazatlan/Mazatlan4.jpeg' ],
@@ -54,7 +58,8 @@ export const Lugares:Casa[]=[
         lugar: ['Mazatlán', 'Sinaloa', 'México'],
         anfitrion: 'Joaquín',
         precio: 1760,
-        ofrece: ['Vista a la playa', 'Wifi', 'Alberca uso compartido', 'Jacuzzi privado', 'Ascensor']
+        ofrece: ['Vista a la playa', 'Wifi', 'Alberca uso compartido', 'Jacuzzi privado', 'Ascensor'],
+        id: 4
     },
     {
         imagen: ['assets/imagenes/Casas/Guanajuato1/Guanajuato3.webp','assets/imagenes/Casas/Guanajuato1/Guanajuato1.webp', 'assets/imagenes/Casas/Guanajuato1/Guanajuato2.webp','assets/imagenes/Casas/Guanajuato1/Guanajuato4.webp', 'assets/imagenes/Casas/Guanajuato1/Guanajuato5.webp'],
@@ -65,7 +70,8 @@ export const Lugares:Casa[]=[
         lugar: ['Guanajuato', 'Guanajuato', 'México'],
         anfitrion: 'Héctor Rodolfo',
         precio: 1200,
-        ofrece: ['Vista a la montaña', 'Vista al jardiín', 'Cocina', 'Wifi', 'Lavadora', 'Tv']
+        ofrece: ['Vista a la montaña', 'Vista al jardiín', 'Cocina', 'Wifi', 'Lavadora', 'Tv'],
+        id: 5
     },
     {
         imagen: ['assets/imagenes/Casas/Tulum/RiveraMaya1.webp', 'assets/imagenes/Casas/Tulum/RiveraMaya2.webp', 'assets/imagenes/Casas/Tulum/RiveraMaya3.webp', 'assets/imagenes/Casas/Tulum/RiveraMaya4.webp', 'assets/imagenes/Casas/Tulum/RiveraMaya5.webp', 'assets/imagenes/Casas/Tulum/RiveraMaya6.webp'],
@@ -76,7 +82,8 @@ export const Lugares:Casa[]=[
         lugar: ['Tulum', 'Quintana Roo', 'México'],
         anfitrion: 'Bellenty',
         precio: 809,
-        ofrece: ['Wifi', 'Acceso al lago', 'Cocina', 'Tv', 'Ascensor']
+        ofrece: ['Wifi', 'Acceso al lago', 'Cocina', 'Tv', 'Ascensor'],
+        id: 6
     },
     {
         imagen: ['assets/imagenes/Casas/Guanajuato/Guanajuato21.webp', 'src/assets/imagenes/Casas/Guanajuato/Guanajuato22.webp',  'src/assets/imagenes/Casas/Guanajuato/Guanajuato23.webp', 'src/assets/imagenes/Casas/Guanajuato/Guanajuato24.webp', 'src/assets/imagenes/Casas/Guanajuato/Guanajuato25.webp', 'src/assets/imagenes/Casas/Guanajuato/Guanajuato26.webp'],
@@ -87,7 +94,8 @@ export const Lugares:Casa[]=[
         lugar: ['Guanajuato', 'Guanajuato', 'México'],
         anfitrion: 'Julieta',
         precio: 1297,
-        ofrece: ['Vista al hirizonte de la ciudad', 'Cocina', 'Wifi', 'Área de trabajo', 'Tv', 'Estacionamiento']
+        ofrece: ['Vista al hirizonte de la ciudad', 'Cocina', 'Wifi', 'Área de trabajo', 'Tv', 'Estacionamiento'],
+        id: 7
 
     },
     {
@@ -99,7 +107,8 @@ export const Lugares:Casa[]=[
         lugar: ['Colima', 'Colima'],
         anfitrion: 'Gaby',
         precio: 755,
-        ofrece: ['Cocina', 'wifi', 'Tv y Netflix', 'Refrigerador']
+        ofrece: ['Cocina', 'wifi', 'Tv y Netflix', 'Refrigerador'],
+        id: 8
     },
     {
         imagen: ['assets/imagenes/Casas/Durango/Durango1.jpeg', 'assets/imagenes/Casas/Durango/Durango2.jpeg','assets/imagenes/Casas/Durango/Durango3.webp','assets/imagenes/Casas/Durango/Durango4.webp','assets/imagenes/Casas/Durango/Durango5.jpeg'],
@@ -110,7 +119,8 @@ export const Lugares:Casa[]=[
         lugar: ['Durango', 'Durango', 'México'],
         anfitrion: 'Victor',
         precio: 561,
-        ofrece: [ 'Cafetera', 'Cocina', 'Wifi', 'Refrigerador', 'Tv con servicios de streaming', 'Área para trabajar']
+        ofrece: [ 'Cafetera', 'Cocina', 'Wifi', 'Refrigerador', 'Tv con servicios de streaming', 'Área para trabajar'],
+        id: 9
     },
     {
         imagen: ['assets/imagenes/Casas/Ciudad Victoria/CV1.webp', 'assets/imagenes/Casas/Ciudad Victoria/CV2.webp','assets/imagenes/Casas/Ciudad Victoria/CV3.webp','assets/imagenes/Casas/Ciudad Victoria/CV4.webp'],
@@ -121,7 +131,8 @@ export const Lugares:Casa[]=[
         lugar: ['Ciudad Victoria', 'Tamaulipas','México'],
         anfitrion: 'Dora Alicia',
         precio: 399,
-        ofrece: [ 'Cafetera', 'Cocina', 'Wifi', 'Aire acondicionado', 'Tv', 'Secadora de pelo', 'Plancha']
+        ofrece: [ 'Cafetera', 'Cocina', 'Wifi', 'Aire acondicionado', 'Tv', 'Secadora de pelo', 'Plancha'],
+        id: 10
     },
     {
         imagen: ['assets/imagenes/Casas/Xochimilco/Xochimilco.webp', 'assets/imagenes/Casas/Xochimilco/Xochimilco1.webp' , 'assets/imagenes/Casas/Xochimilco/Xochimilco2.webp', 'assets/imagenes/Casas/Xochimilco/Xochimilco3.webp'],
@@ -132,7 +143,8 @@ export const Lugares:Casa[]=[
         lugar: ['Xochimilco', 'Ciudad de México', 'México'],
         anfitrion: 'Estefania',
         precio: 1100,
-        ofrece: [ 'Acceso al lago', 'Mesa de ping pong', 'Wifi', 'Tv con servicios de streaming', 'Mini refrigerador', 'Microondas']
+        ofrece: [ 'Acceso al lago', 'Mesa de ping pong', 'Wifi', 'Tv con servicios de streaming', 'Mini refrigerador', 'Microondas'],
+        id: 11
     },
     {
         imagen: ['assets/imagenes/Casas/Nayarit/Nayarit.webp', 'assets/imagenes/Casas/Nayarit/Nayarit1.webp', 'assets/imagenes/Casas/Nayarit/Nayarit2.webp', 'assets/imagenes/Casas/Nayarit/Nayarit3.webp'],
@@ -143,7 +155,8 @@ export const Lugares:Casa[]=[
         lugar: ['Punta Negraa', 'Nayarit','México'],
         anfitrion: 'Cristina Shanti',
         precio: 1946,
-        ofrece: [ 'Vista a la playa', 'Lavadora', 'Wifi', 'Tv', 'Lavadora', 'Refrigerador']
+        ofrece: [ 'Vista a la playa', 'Lavadora', 'Wifi', 'Tv', 'Lavadora', 'Refrigerador'],
+        id: 12
     },
     {
         imagen: ['assets/imagenes/Casas/Visalia/Visalia.webp', 'assets/imagenes/Casas/Visalia/Visalia1.webp', 'assets/imagenes/Casas/Visalia/Visalia2.webp', 'assets/imagenes/Casas/Visalia/Visalia3.webp', 'assets/imagenes/Casas/Visalia/Visalia.webp4', 'assets/imagenes/Casas/Visalia/Visalia.webp5'],
@@ -154,7 +167,8 @@ export const Lugares:Casa[]=[
         lugar: ['Visalia', 'California', 'Estados Unidos'],
         anfitrion: 'Jeanette',
         precio: 2727,
-        ofrece: [ 'Cocina', 'Wifi', 'Área para trabajar', 'Tv', 'Alberca', 'Refrigerador']
+        ofrece: [ 'Cocina', 'Wifi', 'Área para trabajar', 'Tv', 'Alberca', 'Refrigerador'],
+        id: 13
     },
     {
         imagen: ['assets/imagenes/Casas/SF/SF.webp', 'assets/imagenes/Casas/SF/SF1.webp', 'assets/imagenes/Casas/SF/SF2.webp', 'assets/imagenes/Casas/SF/SF3.webp'],
@@ -165,6 +179,7 @@ export const Lugares:Casa[]=[
         lugar: ['San Francisco', 'California', 'Estados Unidos'],
         anfitrion: 'Daniel',
         precio: 1715,
-        ofrece: [ 'Acceso al lago', 'Mesa de ping pong', 'Wifi', 'Tv con servicios de streaming', 'Mini refrigerador', 'Microondas']
+        ofrece: [ 'Acceso al lago', 'Mesa de ping pong', 'Wifi', 'Tv con servicios de streaming', 'Mini refrigerador', 'Microondas'],
+        id: 14
     },
 ]
