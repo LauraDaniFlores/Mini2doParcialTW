34153c77562b84187cce46703c13da872b4b348d
LauraDaniFlores <laura.dani.flores@gmail.com>
2024-05-03 13:36:48 -0600

    Agregar rutas en footer de opciones y reservaciones.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { OpcLugaresComponent } from './opc-lugares/opc-lugares.component';
+import { ReservacionesComponent } from './reservaciones/reservaciones.component';
+import { MenuPrincipalComponent } from '../menu-principal/menu-principal.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'principal', component: MenuPrincipalComponent},
+    {path: 'reservaciones', component: ReservacionesComponent},
+    {path: 'opc-lugares', component: OpcLugaresComponent}, 
+    // {path: 'datos-equipo', component: ReservacionesComponent},
+    // {path: 'acerca-nosotros', component: OpcLugaresComponent}, 
+    {path: '**', pathMatch: 'full', redirectTo: 'principal'}
+];
