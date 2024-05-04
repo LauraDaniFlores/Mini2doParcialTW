263301a65e8f6e4e46a02e5b107c221bcac80d8d
LauraDaniFlores <laura.dani.flores@gmail.com>
2024-05-04 16:58:00 -0600

    Empezar el api y diferente diseño de acerca de nosotros.

--- src/app/acerca-nosotros/acerca-nosotros.component.ts
+++ src/app/acerca-nosotros/acerca-nosotros.component.ts
@@ -1,14 +1,34 @@
 import { Component } from '@angular/core';
 import { DomseguroPipe } from '../domseguro.pipe';
+import { TableModule } from 'primeng/table';
+import { ApiServiceService } from '../service/api-service.service';
 
 @Component({
   selector: 'app-acerca-nosotros',
   standalone: true,
-  imports: [DomseguroPipe],
+  imports: [DomseguroPipe, TableModule],
   templateUrl: './acerca-nosotros.component.html',
   styleUrl: './acerca-nosotros.component.css'
 })
 export class AcercaNosotrosComponent {
   video:string="Gic_CtkJSjE";
 
+  array:any = []; 
+
+  constructor(public apiServiceService: ApiServiceService){}
+
+  recuperarDatos():void{
+    console.log("entre"); 
+
+    this.apiServiceService.retornar().subscribe({
+      next: this.successRequest.bind(this), 
+      error: (err) => {console.log(err)}
+    });
+  }
+
+  successRequest(data:any){
+    console.log(data); 
+    this.array = data.datos; 
+    console.log(this.array); 
+  }
 }
