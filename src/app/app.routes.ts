00890c11d3e481eacb9a7532df13438295347cf2
LauraDaniFlores <laura.dani.flores@gmail.com>
2024-05-03 20:42:44 -0600

    Rutas que faltaban en el footer

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,16 @@
 import { Routes } from '@angular/router';
 import { OpcLugaresComponent } from './opc-lugares/opc-lugares.component';
 import { ReservacionesComponent } from './reservaciones/reservaciones.component';
-import { MenuPrincipalComponent } from '../menu-principal/menu-principal.component';
+// import { MenuPrincipalComponent } from '../menu-principal/menu-principal.component';
+import { DatosEquipoComponent } from './datos-equipo/datos-equipo.component';
+import { AcercaNosotrosComponent } from './acerca-nosotros/acerca-nosotros.component';
+
 
 export const routes: Routes = [
-    {path: 'principal', component: MenuPrincipalComponent},
+    // {path: 'principal', component: MenuPrincipalComponent},
     {path: 'reservaciones', component: ReservacionesComponent},
     {path: 'opc-lugares', component: OpcLugaresComponent}, 
-    // {path: 'datos-equipo', component: ReservacionesComponent},
-    // {path: 'acerca-nosotros', component: OpcLugaresComponent}, 
-    {path: '**', pathMatch: 'full', redirectTo: 'principal'}
+    {path: 'datos-equipo', component: DatosEquipoComponent},
+    {path: 'acerca-nosotros', component: AcercaNosotrosComponent}, 
+    {path: '**', pathMatch: 'full', redirectTo: 'opc-lugares'}
 ];
